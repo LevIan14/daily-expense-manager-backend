da4d4d8401f32477b616cf27591f8f8dafe3f139
galuhayu1 <116106095+galuhayu1@users.noreply.github.com>
2022-11-19 12:09:51 +0700

    Update CategoryRepository.java

--- src/main/java/com/example/DEM/repository/CategoryRepository.java
+++ src/main/java/com/example/DEM/repository/CategoryRepository.java
@@ -10,9 +10,8 @@ import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
-  Category findByCategoryName (String category);
+  Category findByCategoryId (int category);
   List<Category> findByCategoryGroup_CategoryGroupNameAndUserCategory_Username(String categoryGroup, String user);
-  Category findByCategoryId (int Id);
   Category findByCategoryIdAndUserCategory (int categoryId, Optional<User> userCategory);
   void deleteByCategoryId(int id);
 }
