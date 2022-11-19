5a3bef61c922a9ab738aff2a28856e50390c7188
galuhayu1 <116106095+galuhayu1@users.noreply.github.com>
2022-11-19 12:06:34 +0700

    Add files via upload

---
+++ src/main/java/com/example/DEM/model/CategoryRequest.java
@@ -0,0 +1,22 @@
+package com.example.DEM.model;
+
+public class CategoryRequest {
+  private int categoryGroupId;
+  private String name;
+
+  public int getCategoryGroupId() {
+    return categoryGroupId;
+  }
+
+  public void setCategoryGroupId(int categoryGroupId) {
+    this.categoryGroupId = categoryGroupId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
